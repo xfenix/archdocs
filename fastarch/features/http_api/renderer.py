0c56f358aeeb74107c8a159a13e85ebf73282be8
Denis Anikin <ad@xfenix.ru>
2025-03-19 04:29:28 +0300

    I deeply love shitty names

--- fastarch/features/http_api/renderer.py
+++ fastarch/features/http_api/renderer.py
@@ -8,12 +8,12 @@ def draw_http_api_features(service_name: str, features_to_draw: HTTPApiFeatures)
     diagram_parts: list[str] = []
     if features_to_draw.in_methods_existed:
         diagram_parts.append(
-            f"{settings.SHIFT_LEFT}{settings.EXTERNAL_CLIENT_SCHEMA} --> "
+            f"{settings.SHIFT_LEFT}{settings.EXTERNAL_CLIENT_TITLE_FOR_SCHEMA} --> "
             f"|REST ({', '.join(features_to_draw.in_methods)});| {{{service_name}}}",
         )
     if features_to_draw.out_methods_existed:
         diagram_parts.append(
-            f"{settings.SHIFT_LEFT}{settings.EXTERNAL_CLIENT_SCHEMA} <-- "
+            f"{settings.SHIFT_LEFT}{settings.EXTERNAL_CLIENT_TITLE_FOR_SCHEMA} <-- "
             f"|REST ({', '.join(features_to_draw.out_methods)});| {{{service_name}}}",
         )
     return "\n".join(diagram_parts)
