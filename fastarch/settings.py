764747ec98a3ea3a0ddb618eaafd04eab5b363c9
Denis Anikin <ad@xfenix.ru>
2025-04-05 03:44:53 +0300

    HTML layout changed a bit

--- fastarch/settings.py
+++ fastarch/settings.py
@@ -12,19 +12,6 @@ TYPICAL_RE_FLAGS: typing.Final = py_re.IGNORECASE | py_re.MULTILINE | py_re.UNIC
 DEFAULT_PATH: typing.Final = "/docs/architecture/"
 DEFAULT_ROOT_DIR: typing.Final = pathlib.Path()
 DEFAULT_SERVICE_NAME: typing.Final = "example-service"
-UI_HTML_TEMPLATE: typing.Final = (
-    """
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Architecture docs</title>
-        <script src="https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.min.js" async></script>
-    </head>
-    <body>
-        <pre class="mermaid">{}</pre>
-    </body>
-    </html>
-"""
-).strip()
+UI_HTML_TEMPLATE: typing.Final = "".join(
+    pathlib.Path(__file__).parent.joinpath("template.html").read_text().strip().split(),
+)
